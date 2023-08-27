f20525598ce4158c9c2be54e0c797b9bced42bee
Andrew Shearer <andrewshearerdev@gmail.com>
2023-08-27 13:49:14 -0700

    finished styling home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,46 +84,54 @@ const HomePage = async () => {
     <main className="container mx-auto mt-10">
       <h1 className="font-bold mb-10 text-3xl text-center">Shopify + Next.js 13!</h1>
 
-      <h2 className="font-bold text-2xl mb-3">Our Products:</h2>
-      <ul className="grid grid-cols-3 gap-4">
-        {json.data.products.nodes.map((product) => (
-          <li key={product.id} className="border border-slate-200 rounded-md overflow-hidden">
-            <div>
-              <Image
-                src={product.featuredImage.url}
-                alt={product.featuredImage.altText}
-                width={product.featuredImage.width}
-                height={product.featuredImage.height}
-                className="h-96 w-full object-cover"
-              />
-            </div>
+      <div className="px-5">
+        <h2 className="font-bold text-2xl mb-3">Our Products:</h2>
+        <ul className="grid grid-cols-12 gap-4 pb-12">
+          {json.data.products.nodes.map((product) => (
+            <li
+              key={product.id}
+              className="border border-slate-200 rounded-md overflow-hidden col-span-full md:col-span-6 lg:col-span-4"
+            >
+              <div>
+                <Image
+                  src={product.featuredImage.url}
+                  alt={product.featuredImage.altText}
+                  width={product.featuredImage.width}
+                  height={product.featuredImage.height}
+                  className="h-96 w-full object-cover"
+                />
+              </div>
 
-            <div className="p-5">
-              {product.tags.map((tag) => (
-                <span className="bg-yellow-400 font-bold py-1 px-3 rounded-full text-xs" key={tag}>
-                  {tag}
-                </span>
-              ))}
+              <div className="p-5">
+                {product.tags.map((tag) => (
+                  <span
+                    key={tag}
+                    className="bg-yellow-400 font-bold py-1 px-3 rounded-full text-xs"
+                  >
+                    {tag}
+                  </span>
+                ))}
 
-              <h3 className="font-medium mt-3 text-3xl">{product.title}</h3>
+                <h3 className="font-medium mt-3 text-3xl">{product.title}</h3>
 
-              <h4>
-                {formatPrice(product.priceRangeV2.minVariantPrice.amount)}{" "}
-                {product.priceRangeV2.minVariantPrice.currencyCode}
-              </h4>
+                <h4>
+                  {formatPrice(product.priceRangeV2.minVariantPrice.amount)}{" "}
+                  {product.priceRangeV2.minVariantPrice.currencyCode}
+                </h4>
 
-              <p className="my-3">{product.description}</p>
+                <p className="mt-2 mb-4">{product.description}</p>
 
-              <Link
-                href={`/product/${product.handle}`}
-                className="border border-blue-600 inline-block p-2 rounded-md text-blue-600 hover:bg-blue-600 hover:text-white ease-in-out duration-150"
-              >
-                View Product
-              </Link>
-            </div>
-          </li>
-        ))}
-      </ul>
+                <Link
+                  href={`/product/${product.handle}`}
+                  className="border border-blue-600 inline-block p-2 rounded-md text-blue-600 hover:bg-blue-600 hover:text-white ease-in-out duration-150"
+                >
+                  View Product
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 };
